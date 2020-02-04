63e0ce4f4624a17f8d3cb366a04eddc9ca96cde1
Matthew Short <short.matthew.f@gmail.com>
2020-02-04 12:08:07 -0500

    changes

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const COLORS = [
+// 1
+const PALETTE = [
   "red", "orange", "yellow", "green", "blue", "rebeccapurple", "pink", "white", "black"
 ];
 
+// 1
 function makePalette() {
   let palette = $('.palette');
 
-  for (let i = 0; i < COLORS.length; i++) {
+  for (let i = 0; i < PALETTE.length; i++) {
     let button = $('<button>')
-    button.css('backgroundColor', COLORS[i])
+    button.css('backgroundColor', PALETTE[i])
     palette.append(button)
   }
 }
 
 makePalette()
 
+// 1
 function makeGrid() {
   let grid = $('.grid');
 
@@ -25,31 +28,38 @@ function makeGrid() {
 
 makeGrid();
 
+// 2
 $('.palette button').click(function () {
   $('.active').removeClass('active');
   $(this).addClass('active');
 });
 
+// 2
 $('.cell').click(function () {
   let activeColor = $('.palette .active').css('backgroundColor');
   let cellColor = $(this).css('backgroundColor');
 
   if (cellColor === activeColor) {
+    //3
     $(this).css('backgroundColor', '');
   } else {
+    // 2
     $(this).css('backgroundColor', activeColor);
   }
 });
 
+// 2
 $('.clear').click(function () {
   $('.cell').css('backgroundColor', '');
 });
 
+// 2
 $('.fill').click(function () {
   let activeColor = $('.palette .active').css('backgroundColor');
   $('.cell').css('backgroundColor', activeColor);
 })
 
+// 3
 $('.fill-empty').click(function () {
   let activeColor = $('.palette .active').css('backgroundColor');
   let cells = $('.cell');
